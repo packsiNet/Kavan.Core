4d88de2f39d19743c7a109633c46f929390154f9
shahr <shahramoweisy@gmail.com>
2025-11-03 12:37:01 +0330

    Started Project Stabled

--- Kavan.Api/Program.cs
+++ Kavan.Api/Program.cs
@@ -1,20 +1,50 @@
+using AspNetCoreRateLimit;
+using InfrastructureLayer;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.Register(builder.Configuration);
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add SignalR
+builder.Services.AddSignalR();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.WebHost.ConfigureKestrel((context, options) =>
+{
+    options.Configure(context.Configuration.GetSection("Kestrel"));
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger(c =>
+    {
+        c.RouteTemplate = "swagger/{documentName}/swagger.json";
+    });
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/Identity/swagger.json", "API Identity v1");
+        options.SwaggerEndpoint("/swagger/External/swagger.json", "API External v1");
+        options.SwaggerEndpoint("/swagger/Admin/swagger.json", "API Admin v1");
+        options.SwaggerEndpoint("/swagger/Trader/swagger.json", "API Trader v1");
+        options.SwaggerEndpoint("/swagger/Public/swagger.json", "API Public v1");
+    });
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseIpRateLimiting();
+app.UseClientRateLimiting();
+
+app.UseCors("AllowSpecificOrigin");
 
 app.UseAuthorization();
 
