2926e76fd39da96875fe7d9d1ed3efa350632be2
shahr <shahramoweisy@gmail.com>
2025-11-04 16:27:53 +0330

    change nullable

--- ApplicationLayer/Interfaces/Services/Signals/ISignalService.cs
+++ ApplicationLayer/Interfaces/Services/Signals/ISignalService.cs
@@ -1,6 +1,3 @@
-#nullable enable
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using ApplicationLayer.Dto.Signals;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,8 +6,8 @@ namespace ApplicationLayer.Interfaces.Services.Signals
     public interface ISignalService
     {
         Task<List<BreakoutResult>> DetectBreakoutsAsync(
-            List<string>? symbols,
-            List<string>? timeframes,
+            List<string> symbols,
+            List<string> timeframes,
             int lookbackPeriod,
             DbContext db);
     }
