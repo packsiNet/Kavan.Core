7dc7c4ed06ee9066051ca406b3820c1f1fcca70e
AzureDevOpsTeam <azurepanel@yahoo.com>
2025-11-16 22:45:51 +0330

    Improve Code

--- ApplicationLayer/Features/SignalsCatalog/Commands/SeedSignalCatalogCommand.cs
+++ ApplicationLayer/Features/SignalsCatalog/Commands/SeedSignalCatalogCommand.cs
@@ -1,6 +1,5 @@
 using MediatR;
 
-namespace ApplicationLayer.Features.SignalsCatalog.Commands
-{
-    public record SeedSignalCatalogCommand() : IRequest<bool>;
-}
+namespace ApplicationLayer.Features.SignalsCatalog.Commands;
+
+public record SeedSignalCatalogCommand() : IRequest<bool>;
