fc6aaa4b8e1842417fcd3477326f94a1e4d97973
shahr <shahramoweisy@gmail.com>
2025-12-03 08:45:50 +0330

    update cors

--- InfrastructureLayer/DependencyInjection.cs
+++ InfrastructureLayer/DependencyInjection.cs
@@ -57,7 +57,6 @@ public static class DependencyInjection
         services.AddCors(opt => opt.AddPolicy("AllowSpecificOrigin", builder =>
         {
             builder.WithOrigins("https://tg.kavan.trade", "https://core.kavan.trade", "https://panel.kavan.trade", "http://localhost:3003", "http://localhost:5173", "http://localhost:8080", "http://127.0.0.1:3003", "http://127.0.0.1:5173", "http://127.0.0.1:8080", "https://kavan.packsi.net", "https://kavan-core.packsi.net")
-            .SetIsOriginAllowed(origin => true) // Allow any origin for development
             .AllowAnyMethod()
             .AllowAnyHeader()
             .AllowCredentials();
