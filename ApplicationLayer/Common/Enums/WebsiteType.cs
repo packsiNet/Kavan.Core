6368370166b0cfa1962f7ebf00bab370fc8835c6
shahr <shahramoweisy@gmail.com>
2025-12-06 15:11:43 +0330

    feat(Enums): add Official website type to WebsiteType enum
    Add new Official type to support additional website classification needs

--- ApplicationLayer/Common/Enums/WebsiteType.cs
+++ ApplicationLayer/Common/Enums/WebsiteType.cs
@@ -15,9 +15,10 @@ public sealed class WebsiteType
     public static readonly WebsiteType Blog = new(nameof(Blog), 2);
     public static readonly WebsiteType Support = new(nameof(Support), 3);
     public static readonly WebsiteType Docs = new(nameof(Docs), 4);
+    public static readonly WebsiteType Official = new(nameof(Official), 5);
 
     private static readonly HashSet<string> _names = new(StringComparer.OrdinalIgnoreCase)
-    { Main.Name, Blog.Name, Support.Name, Docs.Name };
+    { Main.Name, Blog.Name, Support.Name, Docs.Name, Official.Name };
 
     public static bool IsValid(string name) => string.IsNullOrWhiteSpace(name) || _names.Contains(name);
 }
